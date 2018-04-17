ad4fdce0a37de9955c86ffdad28665cdfa8cde06
carsten <carsten@buchberger-lnx-nb-17.speedport.ip>
2018-04-17 06:51:53 +0200

    Init

---
+++ src/main/java/de/witcom/bpm/example/leaveapproval/LeaveApprovalApplication.java
@@ -0,0 +1,10 @@
+package de.witcom.bpm.example.leaveapproval;
+
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.application.ProcessApplication;
+
+
+@ProcessApplication("Leave Approval App")
+public class LeaveApprovalApplication  extends ServletProcessApplication{
+ 
+}
